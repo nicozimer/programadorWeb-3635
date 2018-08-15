09312e63a01f14f56832ec0322ea7748882d9b6d
nicozimer <nicozimer@gmail.com>
2018-08-14 21:35:09 -0300

    Clase1

---
+++ Clase 01/Alumno/Ejercicio/clase1.js
@@ -0,0 +1,36 @@
+// Punto 1
+
+// var nombre = prompt('Ingresá tu nombre')
+
+// console.log('Hola ' + nombre + '!')
+
+
+// Punto 2
+
+// var numero = prompt('Escribi un numero')
+
+// var parsedNumero = parseFloat(numero, 10)
+
+// console.log('La suma es ' + (parsedNumero + 5))
+
+
+// Punto 3
+
+// var numero = prompt('Escribi un numero')
+
+// var parsedNumero = parseFloat(numero, 10)
+
+// console.log('La resta es ' + (parsedNumero - 5))
+
+
+// Punto 4
+
+var numero = prompt('Escribi un numero')
+
+var numero2 = prompt('Escribi otro numero')
+
+var parsedNumero = parseFloat(numero, 10)
+
+var parsedNumero2 = parseFloat(numero2, 10)
+
+console.log('La suma de ambos es ' + (parsedNumero + parsedNumero2))
