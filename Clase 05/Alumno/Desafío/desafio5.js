61cfbab9427c78e3b3e1dacb9e04452eb22f521a
nicozimer <nicozimer@gmail.com>
2018-09-04 15:14:12 -0300

    cambios clase 5

--- Clase 05/Alumno/Desafío/desafio5.js
+++ Clase 05/Alumno/Desafío/desafio5.js
@@ -13,11 +13,11 @@ function Students (firstName, lastName, dni, email) {
   this.getFullName = function () {
     if (firstName && lastName) {
       return firstName + ' ' + lastName
-    } else if (!firstName&& lastName) {
+    } else if (!firstName && lastName) {
       return lastName
     } else if (firstName && !lastName) {
       return firstName
-    } else if (!firstName&&!lastName) {
+    } else if (!firstName && !lastName) {
       return ''
     }
   }
@@ -29,11 +29,11 @@ var newStudent2 = new Students('Ana', 'Fernandez', 45678989, 'ana@gmail.com')
 
 var newStudent3 = new Students('Pedro', 'Marmol', 45678956, 'pedro@gmail.com')
 
-console.log(newStudent1.getFullName())
-console.log(newStudent1.getID())
+console.log(newStudent1.getFullName(), newStudent1.getID())
 
-console.log(newStudent2.getFullName())
-console.log(newStudent2.getID())
+console.log(newStudent2.getFullName(), newStudent2.getID())
 
-console.log(newStudent3.getFullName())
-console.log(newStudent3.getID())
+console.log(newStudent3.getFullName(), newStudent3.getID())
+
+// // Cambiar las 3 lineas repetidas por un for que recorra el array que nos dieron en la consigna,
+// porque cuando tengamos 50 estudiantes no puede estar poniendo una linea para cada uno.
